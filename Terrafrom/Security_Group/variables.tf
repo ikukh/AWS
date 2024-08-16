ccfecd07453ec4eabfbaf7e49b4f65d51b23c4a9
ikukh <kukharchuk9194@gmail.com>
2024-08-16 13:46:33 +0300

    security group dynamic
    Creation of security group using dynamic group. List of port for inbound and outbound rules created by 'dynamic' with ITERATOR usage

--- Terrafrom/Security_Group/variables.tf
+++ Terrafrom/Security_Group/variables.tf
@@ -1,3 +1,9 @@
 variable "region" {
   default = "eu-central-1"
 }
+
+variable "dynamic_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [8200, 8201,8300, 9200, 9500]
+}
