4b6e1372ae0c25859558a453caebae47f494cdc1
ikukh <kukharchuk9194@gmail.com>
2024-08-17 13:58:39 +0300

    Provisioners added
    TF provisioners used during ec2 creation

--- Terrafrom/EC2/CreateEC2_last_ami.tf
+++ Terrafrom/EC2/CreateEC2_last_ami.tf
@@ -24,6 +24,25 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.ubuntu.image_id
   instance_type = "t2.micro"
+  key_name = "Akey"
+  vpc_security_group_ids = [ "sg-03dff982571027277" ]
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("C:/Users/ikukharc/Downloads/Akey.pem")
+    host = self.public_ip
+  }
+
+provisioner "remote-exec" {
+  inline = [ 
+    "sudo apt update",
+    "yes | sudo apt install nginx",
+    "sudo systemctl start nginx && sudo systemctl enable nginx",
+    "sudo systemctl status nginx >> nginx_status.txt"
+   ]
+}
+
 }
 
 /* get ami-071de147bf3f27475 shold be ami-0e872aee57663ae2d */
--- Terrafrom/Security_Group/variables.tf
+++ Terrafrom/Security_Group/variables.tf
@@ -6,4 +6,4 @@ variable "dynamic_ports" {
   type        = list(number)
   description = "list of ingress ports"
   default     = [8200, 8201,8300, 9200, 9500]
-}
+} 
